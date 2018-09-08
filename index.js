db63285ac01fe9c98dc71cef52336e664c179df6
Bhargava-I <38428230+Bhargava-I@users.noreply.github.com>
2018-09-08 06:21:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
 var getUserMedia = require('getusermedia')
 var nodemailer = require('nodemailer')
-getUserMedia({ video: true, audio: false }, function (err, stream) {
+
+getUserMedia({ video: true, audio: true }, function (err, stream) {
   if (err) return console.error(err)
+
   var Peer = require('simple-peer')
   var peer = new Peer({
     initiator: location.hash === '#init',
     trickle: false,
     stream: stream
   })
+
   peer.on('signal', function (data) {
     document.getElementById('yourId').value = JSON.stringify(data)
-  })
-    document.getElementById('send').addEventListener('click', function () {
-    var sdp = document.getElementById('yourId').value
-    var gm = document.getElementById('gmail').value
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'bhargavbhat95@gmail.com',
-          pass: '9538183813'
-        }
-      })
-      var mailOptions = {
-        from: 'bhargavbhat95@gmail.com',
-        to: gm,
-        subject: 'Sending Email using Node.js',
-        text: sdp
-      }
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    })
   })
   document.getElementById('connect').addEventListener('click', function () {
     var otherId = JSON.parse(document.getElementById('otherId').value)
     peer.signal(otherId)
   })
+
   document.getElementById('send').addEventListener('click', function () {
     var yourMessage = document.getElementById('yourMessage').value
     peer.send(yourMessage)
   })
+
   peer.on('data', function (data) {
     document.getElementById('Received').textContent += data + '\n'
+    document.getElementById('yourMessage').value=''
   })
+
   peer.on('stream', function (stream) {
     var video = document.createElement('video')
     document.body.appendChild(video)
+
     video.src = window.URL.createObjectURL(stream)
     video.play()
   })
